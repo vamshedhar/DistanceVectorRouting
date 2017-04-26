8b0ea6cf27657f37cbc8bf7d1bda18098b8e6969
Vamshedhar Reddy <vamshedhar@gmail.com>
2017-04-25 21:45:15 -0400

    feat(DVP): update read to single protocol

--- src/DistanceVectorRouting.java
+++ src/DistanceVectorRouting.java
@@ -2,56 +2,53 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class DistanceVectorRouting {
-
-	public HashMap<String, NetworkNode> nodes;
 	
-	public DistanceVectorRouting() {
-		this.nodes = new HashMap<String, NetworkNode>();
+	public NetworkNode currentNode;
+	
+	public DistanceVectorRouting(NetworkNode currentNode) {
+		
+		this.currentNode = currentNode;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] NodeList = {"a", "b", "c", "d", "e", "f"};
 		
-		DistanceVectorRouting DVR = new DistanceVectorRouting();
+		String filename = args[0];
 		
-		for(String nodeName : NodeList){
-			DVR.nodes.put(nodeName, new NetworkNode(nodeName, NodeList));
-		}
+		String currentNodeName = filename.split(".txt")[0];
+		
+		NetworkNode currentNode = new NetworkNode(currentNodeName, NodeList);
+		
+		DistanceVectorRouting DVR = new DistanceVectorRouting(currentNode);
 		
 		try{
-			for(String nodeName : NodeList){
-				BufferedReader reader = new BufferedReader(new FileReader(nodeName + ".txt"));
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			
+			String line = reader.readLine();
+			while((line = reader.readLine()) != null) {
 				
-				String line = reader.readLine();
-				while((line = reader.readLine()) != null) {
-					
-					StringTokenizer st = new StringTokenizer(line);
-					
-					if(st.countTokens() != 2){
-						System.err.println("Invalid data!");
-						reader.close();
-						return;
-					}
-					
-					String neighbourNodeName = st.nextToken();
-					Double weight = Double.parseDouble(st.nextToken());
-					
-					NetworkNode node = DVR.nodes.get(nodeName);
-					
-					node.addNeighbourNode(neighbourNodeName, weight, neighbourNodeName);
-					
+				StringTokenizer st = new StringTokenizer(line);
+				
+				if(st.countTokens() != 2){
+					System.err.println("Invalid data!");
+					reader.close();
+					return;
 				}
 				
-				System.out.println(DVR.nodes.get(nodeName));
-				reader.close();
+				String neighbourNodeName = st.nextToken();
+				Double weight = Double.parseDouble(st.nextToken());
+				
+				currentNode.addNeighbourNode(neighbourNodeName, weight, neighbourNodeName);
+				
 			}
 			
-			
+			System.out.println(DVR.currentNode);
+			reader.close();
 		} catch(FileNotFoundException e){
 			System.err.println("File Not Found!");
 			return;
